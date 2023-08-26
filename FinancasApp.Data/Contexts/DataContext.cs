a6f69c9562b46e2ec3dfb52852c8b32d9ffd6b8a
sandro.gama <sandrogamahs@gmail.com>
2023-08-25 21:49:32 -0300

    feat: desenvolvimento do repositorio com EF

---
+++ FinancasApp.Data/Contexts/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using FinancasApp.Data.Entities;
+using FinancasApp.Data.Mappings;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancasApp.Data.Contexts
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = DBFinancasApp; Integrated Security = True; Connect Timeout = 30; Encrypt = False; Trust Server Certificate=False; Application Intent = ReadWrite; Multi Subnet Failover=False");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioMap());
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+        
+    }
+}
---
+++ FinancasApp.Data/Mappings/UsuarioMap.cs
@@ -0,0 +1,55 @@
+﻿using FinancasApp.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace FinancasApp.Data.Mappings
+{
+    //classe de mapeamento ORM para a entidade Usuario
+    public class UsuarioMap : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            //nome da tabela
+            builder.ToTable("USUARIO");
+
+            //chave primária
+            builder.HasKey(x => x.Id);
+
+            //mapeamento dos campos
+            builder.Property(x => x.Id)
+                .HasColumnName("ID")
+                .IsRequired();
+
+            builder.Property(x => x.Nome)
+                .HasColumnName("NOME")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(x => x.Email)
+                .HasColumnName("EMAIL")
+                .HasMaxLength(50)
+                .IsRequired();
+            builder.Property(x => x.Senha)
+                .HasColumnName("SENHA")
+                .HasMaxLength(40)
+                .IsRequired();
+            builder.HasIndex(x => x.Email)
+                .IsUnique();
+            builder.Property(x => x.DataHoraCriacao)
+                .HasColumnName("DATAHORACRIACAO")
+            .HasColumnType("datetime")
+                .IsRequired();
+        }
+    }
+}
+
+
+
+
